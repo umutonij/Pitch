2d69d1960f4fe17bfa59ca810253e261d0328176
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-27 13:00:22 +0200

    add the files in the templates folder

--- app/models.py
+++ app/models.py
@@ -1,34 +1,20 @@
-from . import db 
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-from datetime import datetime 
+from datetime import datetime
 
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(UserMixin, db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True) 
+    username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    pitches = db.relationship('Pitch',backref = 'users',lazy="dynamic")
+    pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-    
-
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_comments(cls,id):
-        reviews = Comment.query.filter_by(pitch_id=id).all()
-        return comments
 
     @property
     def password(self):
@@ -37,85 +23,49 @@ class User(UserMixin, db.Model):
     @password.setter
     def password(self, password):
         self.pass_secure = generate_password_hash(password)
+        
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     def verify_password(self,password):
-            return check_password_hash(self.pass_secure, password)
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
 
 class Pitch(db.Model):
-    '''
-    Pitch class to define Pitch Objects
-    '''
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer,primary_key = True)
-    pitches = db.Column(db.String)
-    category_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    name = db.Column(db.String(255)) 
+    category= db.Column(db.String(255))
     comments = db.relationship('Comment',backref = 'pitches',lazy="dynamic")
-        
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-    def save_pitch(self):
-        '''
-        Function that saves pitches
-        '''
-        db.session.add(self)
-        db.session.commit()
-    
-    @classmethod
-    def get_all_pitches(cls):
-        '''
-        Function that queries the databse and returns all the pitches
-        '''
-        return Pitch.query.all()
-
-    @classmethod
-    def get_pitches_by_category(cls,cat_id):
-        '''
-        Function that queries the databse and returns pitches based on the
-        category passed to it
-        '''
-        return Pitch.query.filter_by(category_id= cat_id)
-
-
-
+    def __repr__(self):
+        return f'User {self.name}'
 
 class Comment(db.Model):
-
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer,primary_key = True)
-    comment= db.Column(db.String)
-    pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    username =  db.Column(db.String)
-    
-    
+    name = db.Column(db.String(255)) 
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+  
 
-    def save_comment(self):
-        '''
-        Function that saves comments
-        '''
-        db.session.add(self)
-        db.session.commit()
+    def __repr__(self):
+        return f'User {self.name}'
 
-    @classmethod
-    def clear_comments(cls):
-        Comment.all_comments.clear()
+# class Review(db.Model):
 
-    @classmethod
-    def get_comments(cls,id):
-        comments = Comment.query.filter_by(pitch_id=id).all()
-
-        return comments
-
-# class Role(db.Model):
-#     __tablename__ = 'roles'
+#     __tablename__ = 'reviews'
 
 #     id = db.Column(db.Integer,primary_key = True)
-#     name = db.Column(db.String(255))
-#     users = db.relationship('User',backref = 'role',lazy="dynamic") 
-
-#     def __repr__(self):
-#         return f'User {self.name}'
+#     movie_id = db.Column(db.Integer)
+#     movie_title = db.Column(db.String)
+#     image_path = db.Column(db.String)
+#     movie_review = db.Column(db.String)
+#     posted = db.Column(db.DateTime,default=datetime.utcnow)
+#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
